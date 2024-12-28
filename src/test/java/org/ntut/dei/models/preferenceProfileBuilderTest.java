f7871813793f98f638f54428d61fb76646375579
huhuhu0420 <ghostcheng.00@gmail.com>
2024-12-28 09:24:12 +0800

    weird

--- src/test/java/org/ntut/dei/models/preferenceProfileBuilderTest.java
+++ src/test/java/org/ntut/dei/models/preferenceProfileBuilderTest.java
@@ -1,29 +1,38 @@
 package org.ntut.dei.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import java.util.Arrays;
+
+import java.util.ArrayList;
 import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
-public class preferenceProfileBuilderTest {
-    private PreferenceProfile preferenceProfile;
-    private List<GenderIdentity> genderIdentities;
-    private List<String> interests;
-
+public class PreferenceProfileBuilderTest {
     @Test
-    public void testBuilder() {
-        genderIdentities = Arrays.asList(new GenderIdentity(GenderIdentityEnum.MALE),
-                new GenderIdentity(GenderIdentityEnum.FEMALE));
-        interests = Arrays.asList("Reading", "Traveling");
-        preferenceProfile = new PreferenceProfileBuilder().setAgeRange(25, 35)
+    public void testPreferenceProfileBuilder() {
+        PreferenceProfileBuilder preferenceProfileBuilder = new PreferenceProfileBuilder();
+        List<String> interests = List.of("testInterest", "testInterest2");
+        List<GenderIdentity> genderIdentities = List.of(new GenderIdentity(GenderIdentityEnum.MALE));
+        PreferenceProfile preferenceProfile = preferenceProfileBuilder
                 .setPreferedGenderIdentity(genderIdentities)
-                .setPreferedInterests(interests).build();
+                .setPreferedInterests(interests)
+                .setAgeRange(10, 20)
+                .build();
 
-        assertNotNull(preferenceProfile);
         assertEquals(genderIdentities, preferenceProfile.getPreferedGenderIdentity());
         assertEquals(interests, preferenceProfile.getPreferedInterests());
-        assertEquals(25, preferenceProfile.getPreferedAgeRange().getMinAge());
-        assertEquals(35, preferenceProfile.getPreferedAgeRange().getMaxAge());
+        assertEquals(10, preferenceProfile.getPreferedAgeRange().getMinAge());
+        assertEquals(20, preferenceProfile.getPreferedAgeRange().getMaxAge());
+    }
+
+    @Test
+    public void testPreferenceProfileBuilderWithDefaultValues() {
+        PreferenceProfileBuilder preferenceProfileBuilder = new PreferenceProfileBuilder();
+        PreferenceProfile preferenceProfile = preferenceProfileBuilder.build();
+
+        assertEquals(List.of(), preferenceProfile.getPreferedGenderIdentity());
+        assertEquals(new ArrayList<>(), preferenceProfile.getPreferedInterests());
+        assertEquals(0, preferenceProfile.getPreferedAgeRange().getMinAge());
+        assertEquals(100, preferenceProfile.getPreferedAgeRange().getMaxAge());
     }
 }
